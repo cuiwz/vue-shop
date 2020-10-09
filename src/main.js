65b1a64ef4699f4b6b6ce0d55715950e3125a9d3
cuiwz <cuiwzupc@163.com>
2020-10-09 23:36:08 +0800

    finish home page

--- src/main.js
+++ src/main.js
@@ -12,6 +12,12 @@ import './assets/css/global.css'
 // Vue原型挂载axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/' // 配置请求根路径
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加 Token 验证的 Authorization 字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 最后必须返回config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
